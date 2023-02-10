10a76114e900f08b68a22a0988840188e74ea0e4
Enzo <e.valfort@epastelsoft.com>
2023-02-10 09:28:46 +0100

    add reservation wip

--- app/controllers/AccountController.php
+++ app/controllers/AccountController.php
@@ -2,53 +2,47 @@
 
 namespace App\Controllers;
 
-use Lib\Database;
 use Lib\Utils;
-use App\classes\Validation;
+use Lib\Session;
+use Lib\Database;
 use App\Entity\User;
 use App\Models\UserModel;
-use Lib\Session;
+use App\classes\Validation;
+use App\Models\SpecialityModel;
 
 class AccountController extends AbstractController
 {
-    // public function registerUser()
-    // {
-    //     if ((new Session("user"))->exist()) {
-    //         return $this->redirectToRoute('app_account_dashboard');
-    //     }
-    //     $title = "Inscription";
-    //     if (Utils::postIsSet($_POST, ["phone"])) {
-    //         $user = new User;
-    //         $user->set("firstName", htmlspecialchars(trim($_POST["fname"])))
-    //             ->set("lastName", htmlspecialchars(trim($_POST["lname"])))
-    //             ->set("email", htmlspecialchars(trim($_POST["email"])))
-    //             ->set("password", htmlspecialchars(trim($_POST["password"])))
-    //             ->set("street", htmlspecialchars(trim($_POST["street"])))
-    //             ->set("postalCode", htmlspecialchars(trim($_POST["postal_code"])))
-    //             ->set("city", htmlspecialchars(trim($_POST["city"])))
-    //             ->set("phone", htmlspecialchars(trim($_POST["phone"])))
-    //             ->set("mobilePhone", htmlspecialchars(trim($_POST["mobile_phone"])));
-    //         $val = new Validation();
-    //         $val->name('Prenom')->value($user->firstName)->pattern('alpha')->min(2)->max(250)->required();
-    //         $val->name('Nom')->value($user->lastName)->pattern('alpha')->min(2)->max(250)->required();
-    //         $val->name('Email')->value($user->email)->pattern('email')->min(2)->max(250)->required();
-    //         $val->name('Mot de Passe (8 caractères, 1 majuscule et 1 minuscule au minimum)')->value($user->password)->pattern('password')->min(8)->max(128)->required();
-    //         $val->name('Rue')->value($user->street)->pattern('address')->min(2)->max(250)->required();
-    //         $val->name('Code Postal')->value($user->postalCode)->pattern('int')->min(5)->max(5)->required();
-    //         $val->name('Ville')->value($user->city)->pattern('alpha')->min(2)->max(100)->required();
-    //         $val->name('Téléphone')->value($user->phone)->pattern('tel');
-    //         $val->name('Téléphone Mobile')->value($user->mobilePhone)->pattern('tel')->required();
-    //         if (!($val->isSuccess())) {
-    //             new Session("flashbag", $val->getErrors());
-    //             return $this->render('account/registerView.phtml', ["title" => $title]);
-    //         }
-    //         $user->set("password", password_hash($user->password, PASSWORD_BCRYPT));
-    //         $userModel = new UserModel();
-    //         $id = $userModel->add($user);
-    //         return ["redirect" => "app_account_login"];
-    //     }
-    //     return $this->render('account/registerView.phtml', ["title" => $title]);
-    // }
+    public function register()
+    {
+        if ((new Session("user"))->exist()) {
+            return $this->redirectToRoute('app_account_dashboard');
+        }
+        $title = "Inscription";
+        if (Utils::postIsSet($_POST, ["phone"])) {
+            $user = new User;
+            $user->set("firstName", htmlspecialchars(trim($_POST["fname"])))
+                ->set("lastName", htmlspecialchars(trim($_POST["lname"])))
+                ->set("email", htmlspecialchars(trim($_POST["email"])))
+                ->set("password", htmlspecialchars(trim($_POST["password"])));
+            $val = new Validation();
+            $val->name('Prenom')->value($user->get("firstName"))->pattern('alpha')->min(2)->max(250)->required();
+            $val->name('Nom')->value($user->get("lastName"))->pattern('alpha')->min(2)->max(250)->required();
+            $val->name('Email')->value($user->get("email"))->pattern('email')->min(2)->max(250)->required();
+            $val->name('Mot de Passe (8 caractères, 1 majuscule et 1 minuscule au minimum)')->value($user->get("password"))->pattern('password')->min(8)->max(128)->required();
+            if((new UserModel)->findBy(["email" => $user->get("email")])){
+                $val->addError("Email déja utilisé");
+            }
+            if (!($val->isSuccess())) {
+                new Session("flashbag", $val->getErrors());
+                return $this->render('account/registerView.phtml', ["title" => $title, "registering" => $user]);
+            }
+            $user->set("password", password_hash($user->get("password"), PASSWORD_BCRYPT));
+            $userModel = new UserModel();
+            $id = $userModel->add($user);
+            return $this->redirectToRoute("app_account_login");
+        }
+        return $this->render('account/registerView.phtml', ["title" => $title]);
+    }
 
     public function login()
     {
@@ -68,6 +62,7 @@ class AccountController extends AbstractController
             if ($user) {
                 new Session("user", $user, ["password"]);
                 unset($user);
+                
                 return $this->redirectToRoute("app_home_main");
             }
         }
@@ -76,29 +71,55 @@ class AccountController extends AbstractController
         ]);
     }
 
-    public function profile()
+    public function edit()
     {
-        $userSession = (new Session("user"));
-        $userModel = new UserModel(new Database);
-        $cats = $userModel->getCats($userSession->get("id"));
-        $dogs = $userModel->getDogs($userSession->get("id"));
-        $cats = Utils::displayAnimalAgeFromBirthday($cats);
-        $dogs = Utils::displayAnimalAgeFromBirthday($dogs);
-        if (!$userSession->exist()) {
-            return ["redirect" => "app_account_login"];
+        $userId = (new Session("user"))->exist() ? (new Session("user"))->get("id") : null;
+        if (!$userId) {
+            return $this->redirectToRoute("app_account_login");
         }
-        $title = "Bonjour" . $userSession->get("firstName") . " " . $userSession->get("lastName");
-        return [
-            "template" =>   [
-                "folder" => "account",
-                "file"   => "dashboard",
-            ],
-            "title" => $title,
-            "cats" => $cats,
-            "dogs" => $dogs
-        ];
+        $user = (new UserModel)->find($userId, true);
+        $specialities = (new SpecialityModel)->findBy([]);
+        if (Utils::postIsSet($_POST)) {
+            $specialityIds = [];
+            foreach ($specialities as $speciality) {
+                $specialityIds[] = $speciality->get('id');
+            }
+            $user
+                ->set("birthdayAt", htmlspecialchars(trim($_POST["birthdayAt"])))
+                ->set("phone", htmlspecialchars(trim($_POST["phone"])))
+                ->set("specialityId", htmlspecialchars(trim($_POST["specialityId"])))
+                ->set("firstName", htmlspecialchars(trim($_POST["firstName"])))
+                ->set("lastName", htmlspecialchars(trim($_POST["lastName"])))
+                ->set("email", htmlspecialchars(trim($_POST["email"])))
+                ->set("password", htmlspecialchars(trim($_POST["password"])));
+            $val = new Validation();
+           
+            $val->name('Date de naissance')->value($user->get('birthdayAt'))->pattern('date_ymd');
+            $val->name('Numéro de téléphone')->value($user->get('phone'))->pattern('text');
+            $val->name('Prénom')->value($user->get('firstName'))->pattern('text')->min(2)->max(250)->required();
+            $val->name('Nom')->value($user->get('lastName'))->pattern('text')->min(2)->max(3000)->required();
+            $val->name('Email')->value($user->get('email'))->pattern('email')->min(2)->max(250)->required();
+            $val->name('Mot de Passe (8 caractères, 1 majuscule et 1 minuscule au minimum)')->value($user->get("password"))->pattern('password')->min(8)->max(128)->required();
+            $val->name('Spécialité')->value($user->get('specialityId'))->inArray($specialityIds);
+            if (!($val->isSuccess())) {
+                new Session("flashbag", $val->getErrors());
+                return $this->render('account/editView.phtml', [
+                    "me" => $user,
+                    'specialities' => $specialities,
+                    'editMode' => true
+                ]);
+            }
+            $userModel = new UserModel();
+            $id = $userModel->add($user);
+            return $this->redirectToRoute('app_attendee_index');
+        }
+        return $this->render('account/editView.phtml', [
+            "me" => $user,
+            'specialities' => $specialities,
+            'editMode' => true
+        ]);
     }
-
+    
     public function logout()
     {
         $userSession = (new Session("user"));
--- app/controllers/AttendeeController.php
+++ app/controllers/AttendeeController.php
@@ -6,82 +6,81 @@ namespace App\Controllers;
 use Lib\Utils;
 use Lib\Session;
 use Lib\Database;
+use Lib\Flashbag;
 use Lib\EnvLoader;
 use App\Entity\User;
+use App\Entity\Attendee;
 use App\Models\UserModel;
+use App\Entity\Speciality;
 use App\classes\Validation;
+use App\Models\AttendeeModel;
+use App\Models\SpecialityModel;
 
-class AccountController extends AbstractController
+class AttendeeController extends AbstractController
 {
+    public function index()
+    {
+        // Utils::dd((new SerieModel())->find(1, true));
+        $flashbag = new Flashbag();
 
+        return $this->render('attendee/indexView.phtml', ["attendees" => (new AttendeeModel)->findBy([], true)]);
+    }
 public function add()
 {
     // Utils::dd((new SerieModel())->find(1, true));
     $flashbag = new Flashbag();
     $editMode = false;
     if (isset($_GET['id']) && (int)htmlspecialchars($_GET['id'])) {
-        $serie = (new SerieModel)->find((int)htmlspecialchars($_GET['id']), true);
+        $attendee = (new AttendeeModel)->find((int)htmlspecialchars($_GET['id']), true);
         $editMode = true;
     }
-    $serie = $serie ?? new Serie;
-    $directors = (new DirectorModel)->findBy([]);
-    $genres = (new GenreModel)->findBy([]);
+    $attendee = $attendee ?? new Attendee;
+    $specialities = (new SpecialityModel)->findBy([]);
+
     if (Utils::postIsSet($_POST)) {
-        $directorIds = [];
-        $genreIds = [];
-        foreach ($directors as $director) {
-            $directorIds[] = $director->get('id');
+        $specialityIds = [];
+        foreach ($specialities as $speciality) {
+            $specialityIds[] = $speciality->get('id');
         }
-        foreach ($genres as $genre) {
-            $genreIds[] = $genre->get('id');
-        }
-        $serie->set("title", htmlspecialchars(trim($_POST["title"])))
-            ->set("description", htmlspecialchars(trim($_POST["description"])))
-            ->set("episodeLength", htmlspecialchars(trim($_POST["episodeLength"])))
-            ->set("numberOfEpisodes", htmlspecialchars(trim($_POST["numberOfEpisodes"])))
-            ->set("directorId", htmlspecialchars(trim($_POST["directorId"])))
-            ->set("genreId", htmlspecialchars(trim($_POST["genreId"])))
-            ->set("idYoutube", htmlspecialchars(trim($_POST["idYoutube"])))
-            ->set("publishedAt", htmlspecialchars(trim($_POST["publishedAt"])));
-        !isset($editMode) || $_FILES["picture"]["name"] != "" ? $serie->set("picture", $_FILES["picture"]) : null;
+        $attendee->set("firstName", htmlspecialchars(trim($_POST["firstName"])))
+            ->set("lastName", htmlspecialchars(trim($_POST["lastName"])))
+            ->set("birthdayAt", htmlspecialchars(trim($_POST["birthdayAt"])))
+            ->set("email", htmlspecialchars(trim($_POST["email"])))
+            ->set("phone", htmlspecialchars(trim($_POST["phone"])))
+            ->set("specialityId", htmlspecialchars(trim($_POST["specialityId"])));
         $val = new Validation();
-        $val->name('Titre')->value($serie->get('title'))->pattern('text')->min(2)->max(250)->required();
-        $val->name('Description')->value($serie->get('description'))->pattern('text')->min(2)->max(3000)->required();
-        $val->name('Durée du film')->value($serie->get('episodeLength'))->pattern('int')->required();
-        $val->name('Durée du film')->value($serie->get('numberOfEpisodes'))->pattern('int')->required();
-        $val->name('Directeur')->value($serie->get('directorId'))->inArray($directorIds)->required();
-        $val->name('Genre')->value($serie->get('genreId'))->inArray($genreIds)->required();
-        $val->name('Id Youtube')->value($serie->get('idYoutube'))->pattern('alphanum')->min(2)->max(100)->required();
-        $val->name('Date de sortie')->value($serie->get('publishedAt'))->pattern('date_ymd');
-        $val->name('Image')->value($serie->get('picture'))->pattern('file', $serie->get('picture'), !$editMode);
+        $val->name('Prénom')->value($attendee->get('firstName'))->pattern('text')->min(2)->max(250)->required();
+        $val->name('Nom')->value($attendee->get('lastName'))->pattern('text')->min(2)->max(3000)->required();
+        $val->name('Date de naissance')->value($attendee->get('birthdayAt'))->pattern('date_ymd')->required();
+        $val->name('Email')->value($attendee->get('email'))->pattern('email')->required();
+        $val->name('Numéro de téléphone')->value($attendee->get('phone'))->pattern('text');
+        $val->name('Spécialité')->value($attendee->get('specialityId'))->inArray($specialityIds)->required();
         if (!($val->isSuccess())) {
             new Session("flashbag", $val->getErrors());
-            return $this->render('serie/newView.phtml', [
-                "serie" => $serie,
-                'genres' => $genres,
-                'directors' => $directors,
+            return $this->render('attendee/newView.phtml', [
+                "attendee" => $attendee,
+                'specialities' => $specialities,
                 'editMode' => $editMode
             ]);
         }
-        if (!isset($editMode) || $_FILES["picture"]["name"]) {
-            $fileExt = '.' . strtolower(pathinfo(basename($_FILES["picture"]["name"]), PATHINFO_EXTENSION));
-            $fileName = date('YmdHis') . uniqid() . $fileExt;
-            $targetFile = ABSOLUTE_ROOT_PATH . EnvLoader::get('UPLOAD_DIR') . $fileName;
-            move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFile);
-            $serie->set('picture', $fileName);
-        }
-        if ($serie->get('createdAt') === null) {
-            $serie->set('createdAt', new \DateTime());
-        }
-        $serie->set('updatedAt', new \DateTime());
-        $serieModel = new SerieModel();
-        $id = $serieModel->add($serie);
-        return $this->redirectToRoute('app_serie_index');
+        // if (!isset($editMode) || $_FILES["picture"]["name"]) {
+        //     $fileExt = '.' . strtolower(pathinfo(basename($_FILES["picture"]["name"]), PATHINFO_EXTENSION));
+        //     $fileName = date('YmdHis') . uniqid() . $fileExt;
+        //     $targetFile = ABSOLUTE_ROOT_PATH . EnvLoader::get('UPLOAD_DIR') . $fileName;
+        //     move_uploaded_file($_FILES["picture"]["tmp_name"], $targetFile);
+        //     $attendee->set('picture', $fileName);
+        // }
+        // if ($attendee->get('createdAt') === null) {
+        //     $attendee->set('createdAt', new \DateTime());
+        // }
+        // $attendee->set('updatedAt', new \DateTime());
+        $attendeeModel = new AttendeeModel();
+        $id = $attendeeModel->add($attendee);
+        return $this->redirectToRoute('app_attendee_index');
     }
-    return $this->render('serie/newView.phtml', [
-        "serie" => $serie,
-        'genres' => $genres,
-        'directors' => $directors,
+    return $this->render('attendee/newView.phtml', [
+        "attendee" => $attendee,
+        'specialities' => $specialities,
         'editMode' => $editMode
     ]);
 }
